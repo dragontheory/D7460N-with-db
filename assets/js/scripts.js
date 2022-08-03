812f96349bb3bc7a50f32fabe7ea6bfc90a45253
D7460N <80736+dragontheory@users.noreply.github.com>
2022-08-03 14:04:24 -0400

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -23,11 +23,11 @@ domReady(() => {
 var barTimeout;
 
 document.body.onscroll = () => {
-	if(barTimeout){
-  	clearTimeout(barTimeout);
+  if(barTimeout){
+    clearTimeout(barTimeout);
   }
   barTimeout = setTimeout(() => {
-  	document.body.classList.remove('scrolling');
+    document.body.classList.remove('scrolling');
   }, 500);
   document.body.classList.add('scrolling');
 }
